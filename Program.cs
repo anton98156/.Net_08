be6e8fa304ed60e4b27a9a92941f9ecdb0d30429
Anton <anton98156789@gmail.com>
2022-11-11 19:45:39 +0300

    Final

--- Program.cs
+++ Program.cs
@@ -154,7 +154,150 @@ else if(SUM4 (array) < SUM1 (array) && SUM4 (array) < SUM2 (array) && SUM4 (arra
 */
 
 
-/* 
-Рекомендованная задача из семинара: 
-Отсортировать нечетные столбцы массива по возрастанию, вывести массив изначальный и отсортированный
+/*
+Задача 58: Задайте две квадратные матрицы. 
+Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+/*
+
+Console.Clear();
+
+Console.WriteLine("Введите размерности матриц: ");
+int m = GetNumbers("Введите кол-во строк в матрицах: ");
+int n = GetNumbers("Введите кол-во столбцов в матрицах: ");
+int range = 10;
+
+int[,] FirstMatrix = new int[m, n];
+GetArray(FirstMatrix);
+Console.WriteLine("Матрица 1: ");
+PrintArray(FirstMatrix);
+
+int[,] SecondMatrix = new int[m, n];
+GetArray(SecondMatrix);
+Console.WriteLine("Матрица 2: ");
+PrintArray(SecondMatrix);
+
+int[,] FinalMatrix = new int[m, n];
+Multiply(FirstMatrix, SecondMatrix, FinalMatrix);
+Console.WriteLine("Произведение матриц: ");
+PrintArray(FinalMatrix);
+
+void Multiply(int[,] FirstMatrix, int[,] SecondMatrix, int[,] FinalMatrix)
+{
+    for (int i = 0; i < FinalMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < FinalMatrix.GetLength(1); j++)
+        {
+            int res = 0;
+            for (int k = 0; k < FirstMatrix.GetLength(1); k++)
+            {
+                res = res + FirstMatrix[i,k] * SecondMatrix[k,j];
+            }
+            FinalMatrix[i,j] = res;
+        }
+    }
+}
+
+int GetNumbers(string Num){
+    Console.WriteLine(Num);
+    int Get = Convert.ToInt32(Console.ReadLine());
+    return Get;
+}
+
+void GetArray (int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = new Random().Next(range);
+        }
+    }
+}
+
+void PrintArray(int [,] array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+*/
+
+// Seminar 09
+
+/*
+**Задача 65:** Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 5, 6, 7, 8"
+*/
+
+/*
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(M, N));
+
+string PrintNumbers(int start, int end){
+    if(start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+*/
+
+/*
+**Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+453 -> 12
+45 -> 9
+*/
+
+/*
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N));
+
+int PrintNumbers(int N){
+    if(N == 0) return (0);
+    return (N % 10 + PrintNumbers(N / 10));
+}
+*/
+
+/*
+**Задача 69:** Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+*/
+
+/*
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+if (N == 0){
+    Console.WriteLine("1");
+}
+else{
+    Console.WriteLine(PrintNumbers(M, N));
+}
+
+int PrintNumbers(int M, int N){
+    if (N == 0) return 1;
+    return (PrintNumbers(M, N - 1) * M);
+}
 */
